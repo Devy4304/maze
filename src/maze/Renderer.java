7cdfdc93929ecddf867ac71d171fed9321bf751c
Devy4304 <148592924+Devy4304@users.noreply.github.com>
2026-01-12 16:15:17 -0800

    Update Renderer and RendererData logic to adjust test data and expand lookup table entries

--- src/maze/Renderer.java
+++ src/maze/Renderer.java
@@ -7,7 +7,7 @@ public class Renderer {
 
     public static int[][] test = {
             {1, 1, 1},
-            {0, 0, 1},
+            {2, 0, 1},
             {1, 0, 1},
             {1, 0, 1},
     };
@@ -36,6 +36,12 @@ public class Renderer {
         // Build draw map
         for (int row = 0; row < area.length; row++) {
             for (int col = 0; col < area[row].length; col++) {
+                int c = switch (col) {
+                    case 0 -> 0;
+                    case 1 -> 2;
+                    case 2 -> 1;
+                    default -> throw new IllegalStateException("Unexpected value: " + col);
+                };
                 int cell = area[row][col];
                 if (cell != 0) {
                     // Create a DrawInfo object
